b268b6592cd3d4bc23b285d05989b79b324fba75
Jason Li <senli@rrc.ca>
2025-09-02 15:39:25 -0500

    update Modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
+import Modal from "./Modal";
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [ modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState(' ');
   const [enteredAuthor, setEnteredAuthor] = useState(' ');
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -15,16 +21,37 @@ function PostsList() {
     setEnteredAuthor(event.target.value);
   }
 
+  // let modalContent;
+
+  // if (modalIsVisible) {
+  //   modalContent = (
+  //     <Modal onClose={hideModalHandler}>
+  //       <NewPost
+  //         onBodyChange={bodyChangeHandler}
+  //         onAuthorChange={authorChangeHandler}
+  //       />
+  //     </Modal>
+  //   );
+  // }
+
   return (
-    <form className={classes.form}>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+    <>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manual" body="I love coding!" />
         <Post author="Maximilian" body="JavaScript is awesome!" />
         <Post author="Manual" body="Let's build something cool!" />
       </ul>
-    </form>
+    </>
   );
 }
 
