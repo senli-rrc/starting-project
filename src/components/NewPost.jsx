12d0aa2902fc636c800ffece68581a4ae28d2ee8
Jason Li <senli@rrc.ca>
2025-09-02 15:01:03 -0500

    [update]useState training complete

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,22 @@
+// import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost() {
+function NewPost(props) {
+  // const [ enteredBody, setEnteredBody ] = useState(' ');
+
+  // function changeBodyHandler(event) {
+  //   setEnteredBody(event.target.value);
+  // }
+  // document.querySelector('textarea').addEventListener('change', function(){})
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState(' ');
+  const [enteredAuthor, setEnteredAuthor] = useState(' ');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React is great!" />
-      <Post author="Manual" body="I love coding!" />
-      <Post author="Maximilian" body="JavaScript is awesome!" />
-      <Post author="Manual" body="Let's build something cool!" />
-    </ul>
+    <form className={classes.form}>
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manual" body="I love coding!" />
+        <Post author="Maximilian" body="JavaScript is awesome!" />
+        <Post author="Manual" body="Let's build something cool!" />
+      </ul>
+    </form>
   );
 }
 
