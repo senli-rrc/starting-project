b831d5d968de46e51b9265eeadb78b577ffa9dec
Jason Li <senli@rrc.ca>
2025-09-03 08:52:49 -0500

    [update]create new post and post on the board

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,52 +5,40 @@ import Modal from "./Modal";
 import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}) {
-  const [ modalIsVisible, setModalIsVisible] = useState(true);
-  const [enteredBody, setEnteredBody] = useState(' ');
-  const [enteredAuthor, setEnteredAuthor] = useState(' ');
+  const [posts, setPosts] = useState([]);
 
-  function hideModalHandler() {
-    setModalIsVisible(false);
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  // let modalContent;
-
-  // if (modalIsVisible) {
-  //   modalContent = (
-  //     <Modal onClose={hideModalHandler}>
-  //       <NewPost
-  //         onBodyChange={bodyChangeHandler}
-  //         onAuthorChange={authorChangeHandler}
-  //       />
-  //     </Modal>
-  //   );
-  // }
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
+            // onBodyChange={bodyChangeHandler}
+            // onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
+            onAddPost={addPostHandler}
           />
         </Modal>
       )}
-
+      {posts.length > 0 && (
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Manual" body="I love coding!" />
+        {/* <Post author="Manual" body="I love coding!" />
         <Post author="Maximilian" body="JavaScript is awesome!" />
-        <Post author="Manual" body="Let's build something cool!" />
+        <Post author="Manual" body="Let's build something cool!" /> */}
+        {posts.map((post) => (
+          <Post key={post.body} author={post.author} body={post.body} />
+        ))}
       </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>No posts found!</h2>
+          <p>Start by creating a new post.</p>
+        </div>
+      )}
     </>
   );
 }
