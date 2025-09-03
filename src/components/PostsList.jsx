9c614e8ad70e36dd0526ca0d54f2216ff9298f19
Jason Li <senli@rrc.ca>
2025-09-03 14:14:57 -0500

    [update]complete Linking and Navigating

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
-import NewPost from "./NewPost";
 import Post from "./Post";
-import Modal from "./Modal";
 import classes from './PostsList.module.css';
 
-function PostsList({isPosting, onStopPosting}) {
+function PostsList() {
   const [posts, setPosts] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -35,16 +33,6 @@ function PostsList({isPosting, onStopPosting}) {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost
-            // onBodyChange={bodyChangeHandler}
-            // onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-            onAddPost={addPostHandler}
-          />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
       <ul className={classes.posts}>
         {/* <Post author="Manual" body="I love coding!" />
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,11 @@
 // import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Modal from '../components/Modal';
 import classes from './NewPost.module.css';
+
 import { useState } from 'react';
 
-function NewPost({onCancel, onAddPost}) {
+function NewPost({ onAddPost }) {
   const [enteredBody, setEnteredBody] = useState(' ');
   const [enteredAuthor, setEnteredAuthor] = useState(' ');
 
@@ -31,20 +34,22 @@ function NewPost({onCancel, onAddPost}) {
   // }
   // document.querySelector('textarea').addEventListener('change', function(){})
   return (
+    <Modal>
       <form className={classes.form} onSubmit={submitHandler}>
         <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
-      </p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={authorChangeHandler} />
-      </p>
-      <p className={classes.actions}>
-        <button type="button" onClick={onCancel}>Cancel</button>
-        <button>Submit</button>
-      </p>
-    </form>
+          <label htmlFor="body">Text</label>
+          <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+        </p>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" required onChange={authorChangeHandler} />
+        </p>
+        <p className={classes.actions}>
+          <Link to=".." type="button">Cancel</Link>
+          <button>Submit</button>
+        </p>
+      </form>
+    </Modal>
   );
 }
 
